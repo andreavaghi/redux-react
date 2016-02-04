eb6083f2a0f2f347cd570269b0dd87d6eb58c22b
Andrea Vaghi <andrea.vaghi@gmail.com>
2016-02-04 07:55:59 +0100

    add video list item

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
-
+import VideoList from './components/video-list'
 const API_KEY = 'AIzaSyBfnUYyHExnU2thTQMb9f-phusTqIjS4ls';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+      return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
