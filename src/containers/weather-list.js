c21ccdbb4bcb8ec719cf56467ab5314d684c313c
Andrea Vaghi <andrea.vaghi@gmail.com>
2016-02-09 08:03:02 +0100

    add weather list and charts

---
+++ src/containers/weather-list.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    const temperatures = cityData.list.map(weather => weather.main.temp);
+    console.log(temperatures);
+    if(cityData.cod === '404') {
+      return;
+    }
+
+    return (
+      <tr key={cityData.city.id}>
+        <td>{cityData.city.name}</td>
+        <td>
+          <Sparklines height={120} width={180} data={temperatures}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
+      </tr>
+    )
+  }
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps({ weather }) {
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
